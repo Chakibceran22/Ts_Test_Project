7b10c0d854464bb8fdbc6515867dd263faaf5d40
Chakibceran22 <grabachakib008@gmail.com>
2025-06-19 20:50:48 +0100

    adding more structure nad more clean code

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 //chakib this is the singleton pattern for prisma client
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
